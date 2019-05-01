aec3bf50c1825f213c015380c51cefa3a86d49b5
github username <github email address>
2019-05-01 17:41:41 -0400

    added serializers

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,4 +1,4 @@
 class Pizza < ApplicationRecord
-  has_many :users, through: :pizza_slices
   has_many :pizza_slices
+  has_many :users, through: :pizza_slices
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :pizzas, through: :pizza_slices
   has_many :pizza_slices
+  has_many :pizzas, through: :pizza_slices
 end
