43bda5115c9705945cb7780e0c9818492b40619d
github username <github email address>
2019-05-02 17:24:30 -0400

    tried to create stuff

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -6,4 +6,18 @@ class PizzasController < ApplicationController
   end
 
 
+  def create
+    byebug
+    @pizzas = Pizza.all
+    @pizza = Pizza.create(pizza_params)
+    render json: @pizzas
+  end
+
+  private
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :vegan, :price)
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
   end
 
 
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+
 end
