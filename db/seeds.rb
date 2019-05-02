4d5a192c4fa6b9beacd76837889b57105f852cd2
github username <github email address>
2019-05-02 11:25:31 -0400

    seeded again

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.find_or_create_by(name: "Matt")
 User.find_or_create_by(name: "Victoria")
+User.find_or_create_by(name: "Richard")
+User.find_or_create_by(name: "Naomi")
+User.find_or_create_by(name: "Akram")
 
 Pizza.find_or_create_by(name: "White", price: 10, vegan: false)
 Pizza.find_or_create_by(name: "Cheese", price: 8, vegan: false)
+Pizza.find_or_create_by(name: "Cheese", price: 9, vegan: false)
+Pizza.find_or_create_by(name: "Vegetable", price: 15, vegan: true)
+Pizza.find_or_create_by(name: "Pepperoni", price: 10, vegan: false)
+Pizza.find_or_create_by(name: "Sausage and Peppers", price: 9, vegan: false)
+Pizza.find_or_create_by(name: "Tofu", price: 14, vegan: true)
+Pizza.find_or_create_by(name: "Cheese", price: 10, vegan: false)
+Pizza.find_or_create_by(name: "BBQ Chicken", price: 12, vegan: false)
+Pizza.find_or_create_by(name: "Pepperoni", price: 20, vegan: false)
 
 
-PizzaSlice.create(user_id: 1, pizza_id: 2)
-PizzaSlice.create(user_id: 2, pizza_id: 1)
-PizzaSlice.create(user_id: 1, pizza_id: 2)
+40.times do
+  PizzaSlice.create(user_id: rand(1..5), pizza_id: rand(1..10))
+end
