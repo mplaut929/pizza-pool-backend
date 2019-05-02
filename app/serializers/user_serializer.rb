9270e2f2487a65ac494c5fd443d30c0db0b6998e
github username <github email address>
2019-05-02 15:48:09 -0400

    finally figured out slices

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name
   has_many :pizzas, through: :pizza_slices
+
+
+
+  # def slices
+  #   User.all.map do |user|
+  #     user.pizza_slices.map do |ps|
+  #       return ps.slices
+  #     end
+  #   end
+  # end
+
 end
