30ebffa30f33a3f16495a7a1429e5c3d19acf433
github username <github email address>
2019-05-06 10:34:07 -0400

    made changes to work with the fetch

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -8,9 +8,17 @@ class PizzasController < ApplicationController
 
   def create
     @pizzas = Pizza.all
-    @pizza = Pizza.create(pizza_params)
+    @pizza = Pizza.find_or_create_by(pizza_params)
     @user = User.find_or_create_by(name: params[:pizza_slices][:user_name])
     @pizza_slice = PizzaSlice.find_or_create_by(user_id: @user.id, pizza_id: @pizza.id, slices: params[:pizza_slices][:slices])
+    render json: @pizza
+  end
+
+  def update
+    @pizzas = Pizza.all
+    @pizza = Pizza.find_or_create_by(pizza_params)
+    @user = User.find_or_create_by(name: params[:pizza_slices][:user_name])
+    @pizza_slice = PizzaSlice.update(user_id: @user.id, pizza_id: @pizza.id, slices: params[:pizza_slices][:slices])
     render json: @pizza
   end
 
